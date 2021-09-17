ac63cf7fab616d837de47f8d4db23b8ccacc29cf
surjya <surjyamaniduan@gmail.com>
2021-09-18 01:48:40 +0530

    wagesaddedwith-a-condition

--- Employee.java
+++ Employee.java
@@ -1,30 +1,34 @@
 package employee;
-import java.util.Random;
+
 
 public class Employee {
 	
-
 	public static void main(String[] args) {
+		int PartTimeHours = 4;
+		int FullDay = 8;
+		int WarkingHourDay = 100;
+		int WorkingDayParMonth = 20;
+		final int empPresent = 1;
+		final int empPartTime = 2;
+		int dailyWage = 0;
 
-		Random rand = new Random();
-
-		int empCheck = rand.nextInt(2);
-
-		System.out.println(empCheck);
-
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+		
 		switch (empCheck) {
-
-		case 0:
-
-			System.out.println("Employee is present");
+		case empPresent:
+			dailyWage = FullDay*WarkingHourDay*WorkingDayParMonth;
+			System.out.println("Employee is Present");
+			System.out.println("Employee Daily 20 Day Wage Is : "+ dailyWage);
 			break;
-
-		default:
-
-			System.out.println("Employee is Absent");
-
+		case empPartTime:{
+			dailyWage = PartTimeHours*WarkingHourDay*WorkingDayParMonth;
+			System.out.println("Employee Part Time is Present");
+			System.out.println("Employee Part Time 20 Day Wage Is : "+ dailyWage);
+			break;
 		}
-
+		default:
+			System.out.println("Employee is Absent");
+			break;
+		}
 	}
-
 }
