5bd1f864c1598c5a544c8eae9a3cd9c580b40f20
surjya <surjyamaniduan@gmail.com>
2021-09-18 01:17:46 +0530

    parttime adding

--- Employee.java
+++ Employee.java
@@ -5,28 +5,30 @@ public class Employee {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation Program");
-		
-		int WAGE_HOURS = 20;
-		int DAILY_HOURS = 8;
-		
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
 
-		System.out.println(empCheck);
+			System.out.println("Welcome to Employee Wage Computation Program");
 
-		if (empCheck == 1) {
+			int WAGE_HOURS = 20;
+			int DAILY_HOURS = 8;
+			int PART_TIME_HOURS = 4;
 
-			System.out.println("Employee is Present");
-			
-			System.out.println("Daily Employee Wage is: "+  WAGE_HOURS * DAILY_HOURS);
-			
-		}
+			int PART_TIME_HOURSS = WAGE_HOURS * PART_TIME_HOURS;
+			int FULL_TIME_HOURS = WAGE_HOURS * DAILY_HOURS;
 
-		else {
-			System.out.println("Employee is Absent");
-		}
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+
+			System.out.println(empCheck);
+
+			if (empCheck == 1) {
+
+				System.out.println("Employee is Full  time: " + FULL_TIME_HOURS);
+			}
+
+			else {
+
+				System.out.println("Employee is Part time :" + PART_TIME_HOURSS);
+			}
 
 	}
-
 }
