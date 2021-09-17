99c3f14df4e2f137e8bf893d6c50da0159432a30
surjya <surjyamaniduan@gmail.com>
2021-09-18 01:11:21 +0530

    Dailywages

--- Employee.java
+++ Employee.java
@@ -6,6 +6,10 @@ public class Employee {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		int WAGE_HOURS = 20;
+		int DAILY_HOURS = 8;
+		
 
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 
@@ -14,6 +18,9 @@ public class Employee {
 		if (empCheck == 1) {
 
 			System.out.println("Employee is Present");
+			
+			System.out.println("Daily Employee Wage is: "+  WAGE_HOURS * DAILY_HOURS);
+			
 		}
 
 		else {
